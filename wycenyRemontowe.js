3ec1bf75b55e650599949a67969a5b5e5f12e7a4
adamm108 <adamm108@gmail.com>
2022-10-04 20:10:40 +0200

    oblicznie pola ściany

--- wycenyRemontowe.js
+++ wycenyRemontowe.js
@@ -3,13 +3,27 @@ const pick = document.getElementById("pick");
 
 var s = document.getElementById("room");
 const addbutton = document.getElementById('add');
+const addwall = document.querySelector("button.addWall");
 
 
     addbutton.addEventListener('click', ()=> {
         const space = [];
+        
      space.push(s.value);
         console.log(space);
+   let spanResult = document.querySelector(' span.addresult');
+
+    spanResult.textContent = space
+    console.log(spanResult)
+
     })
-    let addresult = document.getElementsByClassName("addresult");
-    addresult.textContent = (s)
-    console.log(addresult.textContent)
+  
+addwall.addEventListener('click',()=>{
+    var lenght = document.getElementById("lenght");
+    var height = document.getElementById("height");
+    var result = document.getElementById("result");
+    const wallResult = lenght * height;
+    result.textContent = wallResult;
+    console.log(wallResult)
+} )
+console.log(addwall);
