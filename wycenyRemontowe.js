9e0e4d53508ae85ea3415b1ee84b17d7b0c975fb
adamm108 <adamm108@gmail.com>
2022-10-06 21:05:35 +0200

    Rozwiązany problem z mnożeniem zmiennych

--- wycenyRemontowe.js
+++ wycenyRemontowe.js
@@ -4,7 +4,13 @@ const pick = document.getElementById("pick");
 var s = document.getElementById("room");
 const addbutton = document.getElementById('add');
 const addwall = document.querySelector("button.addWall");
+const add2 = document.getElementById('add2');
+const add3 = document.getElementById('add2');
+var lenght = document.getElementById("lenght");
+var height = document.getElementById("height");
+    var result = document.getElementById("result");
 
+    
 
     addbutton.addEventListener('click', ()=> {
         const space = [];
@@ -16,14 +22,14 @@ const addwall = document.querySelector("button.addWall");
     spanResult.textContent = space
     console.log(spanResult)
 
+    }) 
+    add2.addEventListener('click',()=> {
+    
+        lenghtValue = lenght.value;  
+           
+        heightValue = height.value;
+        let wallResult = parseFloat(lenghtValue) * parseFloat(heightValue);
+        result.textContent = wallResult;
+        console.log(wallResult)
     })
   
-addwall.addEventListener('click',()=>{
-    var lenght = document.getElementById("lenght");
-    var height = document.getElementById("height");
-    var result = document.getElementById("result");
-    const wallResult = lenght * height;
-    result.textContent = wallResult;
-    console.log(wallResult)
-} )
-console.log(addwall);
