2ede9592099d61f3f382856bfb3155c29f79c624
adamm108 <adamm108@gmail.com>
2022-10-17 20:38:00 +0200

    .

--- wycenyRemontowe.js
+++ wycenyRemontowe.js
@@ -2,6 +2,7 @@
 const pick = document.getElementById("pick");
 
 var s = document.getElementById("room");
+var roomresult =[];
 const addbutton = document.getElementById('add');
 const addwall = document.querySelector("button.addWall");
 const add2 = document.getElementById('add2');
@@ -12,19 +13,24 @@ var height = document.getElementById("height");
     var resultWindow = document.getElementById("WindowResult");
 var windowW = document.getElementById('windowwidth')
 var windowH= document.getElementById('windowheight')
+const windowFinal = document.getElementById('windowfinal')
+const wallFinal = document.getElementById('wallfinal')
+
     
 
     addbutton.addEventListener('click', ()=> {
         
      space = s.value;
         console.log(space);
+        roomresult.push(space)
    let spanResult = document.querySelector(' span.addresult');
 
     spanResult.textContent = space
     console.log(spanResult)
+    console.log(roomresult)
 
     }) 
-    add2.addEventListener('click',()=> {
+    add3.addEventListener('click',()=> {
     
         lenghtValue = lenght.value;  
            
@@ -32,13 +38,15 @@ var windowH= document.getElementById('windowheight')
         let wallResult = parseFloat(lenghtValue) * parseFloat(heightValue);
         result.textContent = wallResult;
         console.log(wallResult)
-    })
-    add3.addEventListener('click',()=> {
+
     
         windowWidth = windowW.value;  
            windowHeight = windowH.value;
         let windowResult = parseFloat(windowWidth) * parseFloat(windowHeight);
         resultWindow.textContent = windowResult;
         console.log(windowResult)
+        windowFinal.innerText = windowResult;
+        wallfinal.innerText =parseFloat(wallResult) - parseFloat(windowResult);
     })
+
   
